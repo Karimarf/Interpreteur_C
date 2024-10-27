b42d55c0b10b797e63d911dc6a3d533e9a9c981c
Karimarf <karimaarfaouim@gmail.com>
2024-10-27 09:06:41 +0100

    update main.c

--- src/main.c
+++ src/main.c
@@ -6,59 +6,62 @@
 #include "../include/error.h"
 #include "../include/ast_builder.h"
 
-
-
-
 int main() {
     char input[256];
-    fgets(input, 256, stdin);
-    Token* tokens = lexer(input);
 
-    Token* errors = inputs_error(tokens);
+    while (1) {
+        printf("Entrez une expression (ou tapez 0 pour quitter): ");
+        fgets(input, 256, stdin);
 
+        if (strcmp(input, "0\n") == 0) {
+            printf("Sortie du programme.\n");
+            break;
+        }
 
-    Token* expression_tokens;
+        Token* tokens = lexer(input);
+        Token* errors = inputs_error(tokens);
+        Token* expression_tokens;
 
-    if (tokens[0].type == TOKEN_FONCTION) {
-        expression_tokens = expression_in_fonction_tokens(tokens);
-    } else if (tokens[0].type == TOKEN_TYPE && strcmp(tokens[0].value, "char") == 0) {
-    }else if (tokens[0].type == TOKEN_IDENTIFIER) {
-        expression_tokens = expression_in_identifier(tokens);
+        if (tokens[0].type == TOKEN_FONCTION) {
+            expression_tokens = expression_in_fonction_tokens(tokens);
+        } else if (tokens[0].type == TOKEN_TYPE && strcmp(tokens[0].value, "char") == 0) {
+            continue;
+        } else if (tokens[0].type == TOKEN_IDENTIFIER) {
+            expression_tokens = expression_in_identifier(tokens);
+        } else if (tokens[0].type == TOKEN_TYPE && tokens[2].type == TOKEN_ASSIGN) {
+            expression_tokens = expression_new_identifier(tokens);
+        } else if (tokens[0].type == TOKEN_IDENTIFIER && tokens[1].type == TOKEN_ASSIGN) {
+            expression_tokens = expression_edit_value(tokens);
+        } else {
+            printf("Expression non reconnue.\n");
+            free(tokens);
+            continue;
+        }
+
+        printf("//////////////////////////////////\n");
+
+        Token* Shunting_Yard_expression = shunting_yard(expression_tokens);
+        printf("////////////////////////////////\n");
+
+        for (int i = 0; Shunting_Yard_expression[i].type != TOKEN_EOF; i++) {
+            printf("%s ", Shunting_Yard_expression[i].value);
+        }
+
+        printf("\n\n");
+
+        Node* ast = create_ast(Shunting_Yard_expression);
+        printf("\nPRINT AST\n");
+        printAst(ast);
+
+        printf("\nEVALUATE AST\n");
+        int result = evaluate(ast);
+        printf("\nRESULT: %d\n", result);
+
+        freeAST(ast);
+        free(tokens);
+        free(Shunting_Yard_expression);
+        free(expression_tokens);
     }
-    else if (tokens[0].type == TOKEN_TYPE && tokens[2].type == TOKEN_ASSIGN) {
-        expression_tokens = expression_new_identifier(tokens);
-    } else if (tokens[0].type == TOKEN_IDENTIFIER && tokens[1].type == TOKEN_ASSIGN) {
-        expression_tokens = expression_edit_value(tokens);
-    }
-
-
-    printf("//////////////////////////////////\n");
-
-    Token* Shunting_Yard_expression = shunting_yard(expression_tokens);
-    printf("////////////////////////////////\n");
-
-
-    for (int i = 0; Shunting_Yard_expression[i].type != TOKEN_EOF; i++) {
-        printf("%s", Shunting_Yard_expression[i].value);
-    }
-
-
-    printf("\n\n");
-
-
-    Node* ast = create_ast(Shunting_Yard_expression);
-    printf("\nPRINT AST\n");
-    printAst(ast);
-
-    printf("\nEVALUATE AST\n");
-    int result  = evaluate(ast);
-    printf("\nRESULT: %d\n", result);
-
-    freeAST(ast);
-    free(tokens);
-    free(Shunting_Yard_expression);
-    free(expression_tokens);
-
 
     return 0;
 }
